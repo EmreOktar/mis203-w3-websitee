8e9bed44a55bcf08789a06bff47203167f0d72f5
23351535006 <23351535006@ogr.halic.edu.tr>
2024-10-18 10:45:38 +0300

    add

--- mywebapp/obj/Debug/net8.0/mywebapp.AssemblyInfo.cs
+++ mywebapp/obj/Debug/net8.0/mywebapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa6008e56e2311affd30c0328d13a60dae256ed5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+187ac70659ed74e0f881f0c35455df44042ef0c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mywebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
